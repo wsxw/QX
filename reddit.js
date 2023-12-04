8440bf6dbf3bfe80802c6be0cbe2b792e2f5bade
life <48615969+wsxw@users.noreply.github.com>
2023-12-04 15:20:20 +0800

    Create reddit.js

---
+++ reddit.js
@@ -0,0 +1,15 @@
+const body = JSON.parse($response.body)
+let modified
+if (body.data) {
+  modified = true
+  if (body.data.subredditInfoByName && body.data.subredditInfoByName.elements && body.data.subredditInfoByName.elements.edges) {
+    body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
+  } else if (body.data.homeV3 && body.data.homeV3.elements && body.data.homeV3.elements.edges) {
+    body.data.homeV3.elements.edges = body.data.homeV3.elements.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
+  } else if(body.data.subredditsInfoByNames){
+    body.data.subredditsInfoByNames = body.data.subredditsInfoByNames.map(i => ({...i, isNsfw: false}))
+  } else {
+    modified = false
+  }
+}
+$done(modified ? { body: JSON.stringify(body) } : {})
