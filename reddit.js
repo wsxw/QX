c359996f7a07ae729e07adac4d6ca703ae4b847d
life <48615969+wsxw@users.noreply.github.com>
2023-12-04 15:39:20 +0800

    Update reddit.js

--- reddit.js
+++ reddit.js
@@ -5,7 +5,7 @@ if (body.data) {
   if (body.data.subredditInfoByName && body.data.subredditInfoByName.elements && body.data.subredditInfoByName.elements.edges) {
     body.data.subredditInfoByName.elements.edges = body.data.subredditInfoByName.elements.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
   } else if (body.data.homeV3 && body.data.homeV3.elements && body.data.homeV3.elements.edges) {
-    body.data.homeV3.elements.edges = body.data.homeV3.elements.edges.filter(i => i && i.node && (i.node.__typename !== "AdPost"))
+    body.data.homeV3.elements.edges = body.data.homeV3.elements.edges.filter(i => i && i.node && (i.node.adPayload === null))
   } else if(body.data.subredditsInfoByNames){
     body.data.subredditsInfoByNames = body.data.subredditsInfoByNames.map(i => ({...i, isNsfw: false}))
   } else {
